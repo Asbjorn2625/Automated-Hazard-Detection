9486c64802c7a2d730308392d5277bed87b3cbd8
Miki Pregaard <48087675+Miki0110@users.noreply.github.com>
2023-04-26 14:23:13 +0200

    Found a way to read the text with all the models in the Docker

--- Dockerfile
+++ Dockerfile
@@ -1,27 +1,54 @@
-FROM pytorch/pytorch:latest
+FROM nvidia/cuda:11.8.0-devel-ubuntu22.04
 
-# Install curl and wget
-RUN apt-get update && apt-get install -y curl && \
-    apt-get update && apt-get install -y wget
+# Install curl, wget, and other dependencies
+RUN apt-get update && apt-get install -y \
+    curl \
+    wget \
+    tesseract-ocr \
+    libtesseract-dev \
+    libleptonica-dev \
+    libgl1-mesa-glx \
+    libglib2.0-0 \
+    libgtk2.0-dev \
+    libavcodec-dev \
+    libavformat-dev \
+    libswscale-dev \
+    libtbb-dev \
+    pkg-config
 
-# Set up the working directory
-WORKDIR /app
 
-# Install LibGL used by OpenCV
-RUN apt-get update && apt-get install -y libgl1-mesa-glx
-RUN apt-get update && apt-get install -y libglib2.0-0
-RUN apt-get update && apt-get install -y libgtk2.0-dev libavcodec-dev libavformat-dev libswscale-dev libtbb-dev
+# Add Tesseract repository and install Tesseract
+RUN curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash \
+    && apt-get update \
+    && apt-get install -y tesseract-ocr \
+    tesseract-ocr-eng
 
-# Install pip
-RUN curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && \
-    python get-pip.py && \
-    rm get-pip.py
+# Install Miniconda
+RUN wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.10.3-Linux-x86_64.sh -O miniconda.sh && \
+    bash miniconda.sh -b -p /opt/conda && \
+    rm miniconda.sh
 
-# Set up Python environment
+# Set up the Conda environment
 ENV PATH=/opt/conda/bin:$PATH
-RUN pip install --no-cache-dir -r requirements.txt
+RUN conda update conda -y && \
+    conda create -n myenv python=3.9 -y && \
+    echo "source activate myenv" > ~/.bashrc
 
+# Clean up package lists to reduce image size
+RUN apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+# Copy the requirements file and the rest of the project
 COPY . .
+
+# Install pip dependencies
+RUN /bin/bash -c "source activate myenv && \
+    pip install --no-cache-dir -r requirements.txt && \
+    pip install torch==1.13.1 -f https://download.pytorch.org/whl/cu118/torch_stable.html && \
+    pip install torchvision==0.14.1 torchaudio==0.13.1 && \
+    pip install albumentations && \
+    pip uninstall -y opencv-python opencv-python-headless && \
+    pip install opencv-python==4.5.4.60"
 
 # Set the DISPLAY environment variable to use the host's X server for GUI applications
 ENV DISPLAY=host.docker.internal:0.0
--- testing/text_reader_test.py
+++ testing/text_reader_test.py
@@ -1,13 +1,57 @@
+import sys
+sys.path.append('/workspaces/P6-Automated-Hazard-Detection')
+
 import os
 import numpy as np
 from Libs.Preprocess.processing_functions import resize_image
 import cv2
 from craft_text_detector import Craft
-import torch
 from pytesseract import *
+import torch
+from collections import deque
 
 
-def extract_text(image, box, padding=10):
+# Function to remove edge blobs
+def grassfire(img, start, new_value):
+    rows, cols = img.shape
+    queue = deque()
+    queue.append(start)
+    old_value = img[start]
+
+    while queue:
+        x, y = queue.popleft()
+        if x < 0 or x >= rows or y < 0 or y >= cols:
+            continue
+        if img[x, y] != old_value:
+            continue
+
+        img[x, y] = new_value
+
+        queue.append((x-1, y))
+        queue.append((x+1, y))
+        queue.append((x, y-1))
+        queue.append((x, y+1))
+
+def remove_edge_blobs(img):
+    rows, cols = img.shape
+    new_value = 0 # You can set this value to any number different from the blob's value
+    # Process top and bottom edges
+    for col in range(cols):
+        if img[0, col] > 0:
+            grassfire(img, (0, col), new_value)
+        if img[-1, col] > 0:
+            grassfire(img, (rows-1, col), new_value)
+
+    # Process left and right edges
+    for row in range(rows):
+        if img[row, 0] > 0:
+            grassfire(img, (row, 0), new_value)
+        if img[row, -1] > 0:
+            grassfire(img, (row, cols-1), new_value)
+    return img
+
+
+def extract_text(image, box, display_name, padding=10):
     x1, y1 = box[0]
     x2, y2 = box[1]
     x3, y3 = box[2]
@@ -28,57 +72,65 @@ def extract_text(image, box, padding=10):
     ymin = max(0, ymin)
     xmax = min(w, xmax)
     ymax = min(h, ymax)
-
+    
     cropped_image = image[ymin:ymax, xmin:xmax]
 
     # Convert to grayscale
     gray = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
 
-    scale_factor = 2
+    scale_factor = 3
     resized_image = cv2.resize(gray, (gray.shape[1] * scale_factor, gray.shape[0] * scale_factor), interpolation=cv2.INTER_LANCZOS4)
-
-    blurred_image = cv2.GaussianBlur(resized_image, (3, 3), 0)
 
     # Try Otsu's thresholding instead of adaptive thresholding
     _, thresh = cv2.threshold(resized_image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-
+    
     kernel = np.ones((3, 3), np.uint8)  # Increase the kernel size
     eroded_image = cv2.erode(thresh, kernel, iterations=1)
     dilated_image = cv2.dilate(eroded_image, kernel, iterations=1)
-
+    
+    # Remove edge blobs
+    segmented = remove_edge_blobs(dilated_image)
+    
     # Display the process
-    resize_image(gray, "cropped_image", 0.4)
-    resize_image(thresh, "gray", 0.4)
-    resize_image(dilated_image, "equalized_image", 0.4)
-
+    cv2.imshow(display_name, segmented)
+    
     # Set Tesseract configuration to focus on alphanumeric characters
-    config = '-c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789./- --psm 4 --oem 3'
-    text = pytesseract.image_to_string(dilated_image, config=config)
+    config = '-c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789./- --psm 6 --oem 3'
+    text = pytesseract.image_to_string(segmented, config=config)
     return text.strip()
 
-
 # Create list of image filenames
-rgb_images = [f'./text_test/{img}' for img in os.listdir("./text_test") if img.startswith("rgb_image")]
+rgb_images = [f'./testing/text_test/{img}' for img in os.listdir("./testing/text_test") if img.startswith("rgb_image")]
 
 # Load the pre-trained CRAFT model
 craft = Craft("cuda:0" if torch.cuda.is_available() else "cpu")  # Use GPU if available, otherwise use CPU
-# Load the OCR model
-pytesseract.tesseract_cmd = "C:\Program Files\Tesseract-OCR\\tesseract.exe"
 
 # Loop through images
 for image in rgb_images:
     img = cv2.imread(image)
+    depth = np.fromfile(image.replace("rgb_image", "depth_image").replace("png", "raw"), dtype=np.uint16)
+    # Reshape the depth image
+    depth = depth.reshape(int(1080), int(1920))
+    # Cut out the background
+    mask = np.where((depth > 1000) | (depth < 10), 0, 255).astype(np.uint8)
+    # Dialate the mask
+    kernel = np.ones((9, 9), np.uint8)
+    mask = cv2.dilate(mask, kernel, iterations=2)
+    # Apply the mask to the image
+    img = cv2.bitwise_and(img, img, mask=mask)
     # Rotate image
     img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
     orig = img.copy()
-
+    
     # Detect text regions
     prediction_result = craft.detect_text(img)
 
     # Draw the bounding boxes on the image
     detected_texts = {"text": [], "box": []}
+    temp_point = 0
     for box in prediction_result["boxes"]:
-        text = extract_text(orig, box)
+        text = extract_text(orig, box, f'dialated_{temp_point}')
+        temp_point += 1
         if text:
             box = np.array(box, dtype=np.int32)  # Convert box coordinates to int32
             # Append the detected text and its bounding box to the list
