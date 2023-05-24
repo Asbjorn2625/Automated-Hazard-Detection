8533a96b29dae9680a92bdca3578de02ab8d7a61
Asbjørn Lauritsen <asbjorn2625@gmail.com>
2023-05-24 17:11:02 +0200

    Fixed some of the tests

--- testing/Orientation_test.py
+++ testing/Orientation_test.py
@@ -4,66 +4,90 @@ sys.path.append('/workspaces/Automated-Hazard-Detection')
 
 from src.Preprocess.prep import PreProcess
 from src.Data_acquisition.Image_fetcher import ImageFetcher
-from src.Classification.Classy import classifier
+from src.Classification.Classy import Classifier
 from src.Segmentation.segmentation import Segmentation
 import pandas as pd
 import cv2
 import numpy as np
+from src.Text_reader.ReaderClass import ReadText
 
-classy = classifier()
+read = ReadText()
+pp= PreProcess()
+classy = Classifier(read, pp)
 Imagefetch = ImageFetcher(os.getcwd() + "/Dataset")
 imglib = Imagefetch._Orientation_test()
-pp= PreProcess()
-seg = Segmentation(model_type="CombinedLoss")
+
 def format_degree(value):
         return f"{value}$^\circ$"
 
 
-columns = { "Real angle":[], "Predicted angle": [],"difference": []}
+columns = {"Real angle":[], "label1 Pred angle": [],"label2 Pred angle": [],"Difference": []}
 
-
-
-for filename, (rgb_img, depth_img) in imglib.items():
-    # undistort the image
-    img = pp.undistort_images(rgb_img)
+list = os.listdir(os.getcwd() + "/Dataset")
+real_angles= []
+pred1 = []
+pred2 = []
+for masks in list :
+    mask = cv2.imread(os.getcwd()+ "/Dataset/" + masks)
+    mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    ret, mask = cv2.threshold(mask, 20, 255, cv2.THRESH_BINARY)
     
-    depth = pp.undistort_images(depth_img)
-    
-    depth_blurred = cv2.medianBlur(depth, 5)
-    
-    trans_img, homography = pp.retrieve_transformed_plane(img, depth_blurred)
-    segmented_img=seg.locateHazard(trans_img)
-    ROI = pp.segmentation_to_ROI(segmented_img)
-    
-    for bounds in ROI:
-        image = segmented_img[bounds[1]:bounds[3], bounds[0]:bounds[2]]
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
+    image = cv2.rotate(mask, cv2.ROTATE_90_CLOCKWISE)
+    resized = cv2.resize(image, (int(image.shape[1]*0.5),int(image.shape[0]*0.5)), interpolation = cv2.INTER_AREA)
 
-        resized = cv2.resize(trans_img, (int(trans_img.shape[1]*0.5),int(trans_img.shape[0]*0.5)), interpolation = cv2.INTER_AREA)
-        
-        cv2.imshow("image", image)
-        cv2.imshow("trans",resized)
-        cv2.waitKey(0)
-    if "+" in filename:
-        real_angle = float(filename.split("+")[1])
-        columns["Real_angle"].append(format_degree(round(real_angle,2)))
-        prediction= abs(classy.Orientation(image))
-        print(round(prediction,2))
-        columns["Predicted_angle"].append(format_degree(round(prediction,2)))
-        columns["diffrence"].append("\\textbf{"+ format_degree(round(abs(real_angle - prediction),2))+ "}")
-        
-    elif "-" in filename:
-        real_angle = float(filename.split("-")[1])
-        columns["Real_angle"].append(format_degree(round(real_angle,2)))
-        prediction= abs(classy.Orientation(image))
-        print(round(prediction,2))
-        columns["Predicted_angle"].append(format_degree(round(prediction,2)))
-        columns["diffrence"].append("\\textbf{"+ format_degree(round(abs(real_angle - prediction),2))+ "}")
-    else:
-        continue
+    if "Explosive" in masks:
+        if "+" in masks:
+            real_angle = float(masks.split("+")[1].replace(".png",""))
+            columns["Real angle"].append(format_degree(round(real_angle,2)))
+            prediction= abs(classy.Orientation(image))
+            pred1.append(prediction)
+            print(prediction)
+            cv2.imshow("Explosive+", resized)
+            cv2.waitKey(0)
+            columns["label1 Pred angle"].append(format_degree(round(prediction,2)))
+            
+        elif "-" in masks:
+            real_angle = float(masks.split("-")[1].replace(".png",""))
+            columns["Real angle"].append(format_degree(round(real_angle,2)))
+            prediction= abs(classy.Orientation(image))
+            pred1.append(prediction)
+            print(prediction)
+            cv2.imshow("Explosive-", resized)
+            cv2.waitKey(0)
+            columns["label1 Pred angle"].append(format_degree(round(prediction,2)))
+        else:
+            continue
+    if "Oxidant" in masks:
+        if "+" in masks:
+            print(masks)
+            real_angle = float(masks.split("+")[1].replace(".png",""))
+            columns["Real angle"].append(format_degree(round(real_angle,2)))
+            prediction= abs(classy.Orientation(image))
+            pred2.append(prediction)
+            print(prediction)
+            cv2.imshow("Oxidant+", resized)
+            cv2.waitKey(0)
+            columns["label2 Pred angle"].append(format_degree(round(prediction,2)))
+            
+        elif "-" in masks:
+            print(masks)
+            real_angle = float(masks.split("-")[1].replace(".png",""))
+            columns["Real angle"].append(format_degree(round(real_angle,2)))
+            prediction= abs(classy.Orientation(image))
+            pred2.append(prediction)
+            print(prediction)
+            cv2.imshow("Oxidant-", resized)
+            cv2.waitKey(0)
+            columns["label2 Pred angle"].append(format_degree(round(prediction,2)))
+        else:
+            continue
+real = [value.replace("$^\circ$", "") for value in columns["Real angle"][:len(columns["Real angle"]) // 2]]
 
 
+for i in range(len(real)):
+    columns["Difference"].append(format_degree(round(abs(((float(real[i]) - pred1[i])+(float(real[i])-pred2[i])/2)),2)))
+    
+columns["Real angle"] = columns["Real angle"][:len(columns["Real angle"]) // 2]
 
 df = pd.DataFrame(columns)
 print(df.to_latex(index=False,escape=False))
--- testing/classi_test.py
+++ testing/classi_test.py
@@ -10,7 +10,9 @@ import Levenshtein
 
 sys.path.append('/workspaces/Automated-Hazard-Detection')
 from src.Preprocess.prep import PreProcess
+from src.Segmentation.segmentation import Segmentation
 from src.Text_reader.ReaderClass import ReadText
+from src.Classification.Classy import Classifier
 
 
 def find_closest_match(input_list, DGRlist):
@@ -50,14 +52,15 @@ def find_closest_match(input_list, DGRlist):
     else:
         return scores, ""
 
-list = os.listdir(os.getcwd() + "/Dataset")
 RT = ReadText()
 PP = PreProcess()
+seg= Segmentation()
+classy = Classifier(RT,PP)
 columns = {"Real text":[],"prediction":[]}
 
 test = {"Explosives": ["EXPLOSIVE","1", "1.1","1.2","1.3","1.4","1.5","1.6"],
         "Flammable Gas": ["FLAMMABLE","GAS","FLAMMABLE GAS","2","2.1"],
-        "Non-flammable gas": ["NON-FLAMMABLE","GAS","2","2.1"],
+        "Non-Flammable Gas": ["NON-FLAMMABLE","GAS","2","2.1"],
         "Toxic Gas": ["TOXIC","GAS","TOXIC GAS","2","2.3"],
         "Flammable Liquid": ["Flammable Liquid","Flammable", "Liquid","3"],
         "Flammable Solid": ["FLAMMABLE", "SOLID", "FLAMMABLE SOLID","4","4.1"],
@@ -66,64 +69,113 @@ test = {"Explosives": ["EXPLOSIVE","1", "1.1","1.2","1.3","1.4","1.5","1.6"],
         "Oxidizing Agent": ["OXIDISING","AGENT","OXIDISING AGENT","5","5.1"],
         "Organic Peroxides": ["ORGANIC", "PEROXIDES","ORGANIC PEROXIDES","5","5.2"],
         "Toxic": ["TOXIC","6","6.1"],
-        "Infectous Substance": ["INFECTIOUS","SUBSTANCE","INFECTIOUS SUBSTANCE", "6","6.2"],
+        "Infectious Substance": ["INFECTIOUS","SUBSTANCE","INFECTIOUS SUBSTANCE", "6","6.2"],
         "Corrosive": ["CORROSIVE","8"],
         "Miscellanous": ["MISCELLANEOUS","9"],
         "Lithium Batteries": ["LITHIUM BATTERIES", "9"]}
 
-ground_truth = ["Flammable Liquid","Flammable Solid","Toxic Gas","Spontaneosly Combustible","Toxic","Oxidizing Agent","Corrosive","Explosives","Infectous Substance"]
-length = len(ground_truth)*4
-for items in ground_truth:
-    for i in range(4):
-        columns["Real text"].append(items)
+ground_truth = []
+image_folder = os.getcwd() + "/Dataset"
 
-for filename in tqdm(list):
+image_paths = [os.path.join(image_folder, img) for img in os.listdir(os.getcwd() + "/Dataset") if img.endswith("_MASK.png")]
+
+
+for path in tqdm(image_paths):
     text1=[]
-    if filename.endswith("_MASK.png"):
-        mask_img=cv2.imread(os.getcwd()+ "/Dataset/" + filename, 0)
-        mask_img = cv2.threshold(mask_img, 10, 255, cv2.THRESH_BINARY)[1]
-        rgb_img=cv2.imread(os.getcwd()+ "/Dataset/" + filename.replace("_MASK",""))
-        masked=cv2.bitwise_and(rgb_img, rgb_img, mask=mask_img)
-        ROI=PP.segmentation_to_ROI(mask_img)
+    imgpath = path.replace("_MASK","")
+    ground_truth.append(path.split("/")[-1].split("_")[0])
+    img = cv2.imread(imgpath)
+    mask = cv2.imread(path, 0)
+    mask = cv2.threshold(mask, 10, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    masked=cv2.bitwise_and(img, img, mask=mask)
+    if classy.labels_on_edge(mask):
+        columns["prediction"].append("")
+    else:
+        ROI=PP.segmentation_to_ROI(mask)
         for bounds in ROI:
             cropped = masked[bounds[1]:bounds[3], bounds[0]:bounds[2]]
-            RT.findText(cropped)
             bounding = RT.findText(cropped)
-            config='-c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789\ ./- --psm 7 --oem 3'
             for boxes in bounding:
-                predtext= RT.readText(cropped, boxes,config=config)
+                if "Explosives" in imgpath:
+                    predtext= RT.readText(cropped, boxes,display= True)
+                    print(path)
+                    print(predtext)
+                else:
+                    predtext= RT.readText(cropped, boxes)
                 text1.append(predtext)
-                
-                            
         if len(text1) == 0:
-            columns["Predicted text"].append("No text found")
+            columns["prediction"].append("No text found")
         else:
             columns["prediction"].append(find_closest_match(text1,test)[1])
-    else:
-        continue
+columns["Real text"] = ground_truth
+
 for i, value in enumerate(columns["prediction"]):
     if value != "":
         if len(value) >= 2:
             columns["prediction"][i] = ""
         else:
             columns["prediction"][i] = value[0]
-            
-cm=confusion_matrix(columns["Real text"],columns["prediction"],labels=ground_truth)
-print(cm)
+for i, value in enumerate(columns["prediction"]):
+    if value == "":
+        columns["prediction"][i] = "Bent"
 
 
-class_labels= ["FL","FS","TG","SC","TX","OA","CO","EX","IS"]
-# Plot confusion matrix
-plt.imshow(cm, cmap='Blues')
-plt.title('Confusion Matrix')
-plt.colorbar()
-tick_marks = np.arange(len(class_labels))
-plt.xticks(tick_marks, class_labels)
-plt.yticks(tick_marks, class_labels)
+if len(columns["Real text"]) == len(columns["prediction"]):
+    df = pd.DataFrame(columns)
+    print(df.to_html(index=False))
+else:
+    print(columns)
+
+unique_labels = set(ground_truth)
+shortening = {"Bent": "F","Corrosive": "Co", "Dangerous When Wet": "DW", "Explosives": "EX", "Flammable Gas": "FG", "Flammable Liquid": "FL", "Flammable Solid": "FS", "Infectious Substance": "IS", "Lithium Batteries": "LB", "Miscellanous": "M", "Non-Flammable Gas": "NF", "Organic Peroxides": "OP", "Oxidizing Agent": "OA", "Spontaneously Combustible": "SC", "Toxic": "T", "Toxic Gas": "TG"}
+class_labels = [shortening.get(item, item) for item in unique_labels]
+
+unique_labels = list(set(columns["Real text"] + columns["prediction"]))
+if "Bent" not in unique_labels:
+    unique_labels.append("Bent")
+
+cm = confusion_matrix(columns["Real text"], columns["prediction"], labels=unique_labels)
+
+# Find the index of the "Fail" class label
+fail_index = unique_labels.index("Bent")
+
+# Rearrange the class labels, moving the "Fail" label to the last position
+class_labels_reordered = class_labels[:fail_index] + class_labels[fail_index+1:] + [class_labels[fail_index]]
+
+# Rearrange the rows of the confusion matrix
+cm_reordered = np.concatenate((cm[:fail_index], cm[fail_index+1:], cm[fail_index:fail_index+1]), axis=0)
+
+# Rearrange the columns of the confusion matrix
+cm_reordered = np.concatenate((cm_reordered[:,:fail_index], cm_reordered[:,fail_index+1:], cm_reordered[:,fail_index:fail_index+1]), axis=1)
+
+# Plot the confusion matrix with the reordered class labels and matrix
+plt.imshow(cm_reordered, cmap='Blues', interpolation='nearest')
+
+# Add text annotations for each cell in the confusion matrix
+for i in range(len(class_labels_reordered)):
+    for j in range(len(class_labels_reordered)):
+        plt.text(j, i, str(cm_reordered[i][j]), ha='center', va='center', color='white')
+
+# Set the tick marks and labels
+tick_marks = np.arange(len(class_labels_reordered))
+plt.xticks(tick_marks, class_labels_reordered, rotation='vertical')
+plt.yticks(tick_marks, class_labels_reordered)
+
+# Add labels and title
 plt.xlabel('Predicted')
 plt.ylabel('True')
-plt.grid(True) 
-plt.show()
+plt.title('Confusion Matrix')
 
-df = pd.DataFrame(columns)
-print(df.to_html(index=False))
+# Display the color bar
+plt.colorbar()
+
+# Draw vertical grid lines
+for i in range(len(class_labels)-1):
+    plt.axvline(x=i+0.5, color='black', linewidth=1)
+
+# Draw horizontal grid lines
+for i in range(len(class_labels)-1):
+    plt.axhline(y=i+0.5, color='black', linewidth=1)
+
+# Show the plot
+plt.show()
